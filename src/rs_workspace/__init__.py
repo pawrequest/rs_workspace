3a6560c25e40a1db28e710f00485ab7365a843e3
paw <pawrequest@users.noreply.github.com>
2025-02-09 17:30:17 +0000

    .

--- src/rs_workspace/__init__.py
+++ src/rs_workspace/__init__.py
@@ -1,4 +1,20 @@
 from rs_workspace.setup_workspace import rs_init
-from rs_workspace.project_manager import install_mod_from_config_json, install_mod, make_config
+from rs_workspace.project_manager import (
+    install_mod,
+    install_mod_from_config_json,
+    make_config,
+)
 
-all = [rs_init, install_mod_from_config_json, install_mod, make_config]
+
+def rs_init_install():
+    rs_init()
+    install_mod_from_config_json()
+
+
+all = [
+    rs_init,
+    install_mod_from_config_json,
+    install_mod,
+    make_config,
+    rs_init_install,
+]
