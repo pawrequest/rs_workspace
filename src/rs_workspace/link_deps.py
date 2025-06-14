8ae2832b2978b117460e4610fde93267e79960ad
paw <pawrequest@users.noreply.github.com>
2025-06-14 01:52:29 +0100

    launch in new process + trim 0.0.9

--- src/rs_workspace/link_deps.py
+++ src/rs_workspace/link_deps.py
@@ -1,3 +1,4 @@
+"""Copy SymLinks for dependencies into the local directory... like a rustic VsCode Workspace or IntelliJ Project"""
 from pathlib import Path
 
 from rs_workspace.paths import GAME_DIR, R4EXT, R4EXT_PLUGINS, R6, R6_SCRIPTS, DEPS_DIR
@@ -29,20 +30,3 @@ def install_deps_symlinks(deps_dir: Path = DEPS_DIR, game_dir: Path = GAME_DIR, 
     make_links(game_dir / R6_SCRIPTS, deps_dir / R6, exclude=exclude)
     make_links(game_dir / R4EXT_PLUGINS, deps_dir / R4EXT, exclude=exclude)
 
-
-#####
-
-
-# def install_deps_symlinks1(deps_dir: Path, game_dir: Path = None, exclude: set = None):
-#     """Install symlinks to the game directory for r6 and r4ext"""
-#     game_dir = game_dir or game_dir_from_env()
-#     game_dir = Path(game_dir)
-#     exclude = exclude or set()
-#
-#     r6_dir = game_dir / 'r6' / 'scripts'
-#     r4ext_dir = game_dir / 'red4ext' / 'plugins'
-#     r6_out = deps_dir / 'r6'
-#     r4ext_out = deps_dir / 'r4ext'
-#
-#     make_links(r6_dir, r6_out, exclude=exclude)
-#     make_links(r4ext_dir, r4ext_out, exclude=exclude)
